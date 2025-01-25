009fd976b56838c99d86b5429b0a22256bd458d1
Adil023 <edilqedirov023@gmail.com>
2025-01-26 01:18:02 +0400

    i solved min max problem

---
+++ src/MinMaxSum.java
@@ -0,0 +1,19 @@
+import java.util.Collections;
+import java.util.List;
+
+public class MinMaxSum {
+
+    public static void minmaxsum(List<Integer> arr){
+     int min = Collections.min(arr);
+     int max =   Collections.max(arr);
+     int totalSum =0;
+     for(int num:arr){
+         totalSum += num;
+     }
+
+     int minSum = totalSum - max;
+     int maxSum = totalSum - min;
+        System.out.println(minSum+" "+maxSum);
+    }
+}
+
