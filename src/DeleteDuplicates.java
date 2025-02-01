15ccccff2382bd285ff73e44e960311c01d68b34
Adil023 <edilqedirov023@gmail.com>
2025-02-02 01:30:13 +0400

    DeleteDuplicates

---
+++ src/DeleteDuplicates.java
@@ -0,0 +1,33 @@
+public class DeleteDuplicates {
+    public ListNode deleteDuplicates(ListNode head) {
+         if(head == null || head.next ==null) return head;
+
+         ListNode listNode = head;
+         while(listNode.next != null){
+             if(listNode.val == listNode.next.val){ //1 1
+                  listNode.next = listNode.next.next;
+             }else{
+                 listNode = listNode.next;
+             }
+
+         }
+         return head;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode() {
+        }
+
+        public ListNode(int value) {
+            this.val = value;
+        }
+
+        public ListNode(int value, ListNode next) {
+            this.val = value;
+            this.next = next;
+        }
+    }
+}
