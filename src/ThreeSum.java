c7e1cc1bcede1d0ee9d039ecdf28ce1882362d36
Adil023 <edilqedirov023@gmail.com>
2025-02-04 13:14:37 +0400

    threeSum

---
+++ src/ThreeSum.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class ThreeSum {
+    public static void main(String[] args) {
+        int[] arr = {-1, 0, 1, 2, -1, -4};
+        System.out.println(findThreeSum(arr));
+    }
+
+    public static List<List<Integer>> findThreeSum(int[] arr){
+        Set<List<Integer>> nums = new HashSet<>();
+
+
+        Arrays.sort(arr);
+
+        for(int i=0;i<arr.length-2;i++){
+            int left=i+1;
+            int right=arr.length-1;
+            int sum =  arr[i]+arr[left]+arr[right];
+            while(left < right){
+               if(sum == 0){
+                   nums.add(Arrays.asList(arr[i],arr[left],arr[right]));
+                   left++;
+                   right--;
+               }else if(sum<0){
+                   left++;
+               }else{
+                   right--;
+               }
+           }
+
+        }
+        return new ArrayList<>(nums);
+    }
+}
