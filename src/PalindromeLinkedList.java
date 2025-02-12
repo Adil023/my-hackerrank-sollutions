1d32ebea333df3950cce33df5b7e816572aa7df8
Adil023 <edilqedirov023@gmail.com>
2025-02-12 13:39:53 +0400

    palindrome linkedlist

---
+++ src/PalindromeLinkedList.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        if(fast != null){
+            slow = slow.next;
+        }
+
+
+        slow = reverseLinkedList(slow);
+        fast = head;
+
+        while(slow != null){
+            if(slow.val != fast.val){
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next;
+
+        }
+
+        return true;
+    }
+
+    public static ListNode reverseLinkedList(ListNode head){
+        ListNode prev = null;
+        while(head != null){
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+
+        return prev;
+    }
+}
